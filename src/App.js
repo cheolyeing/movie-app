80f8be375d31d541a216f77cb5b174703369b427
임희철\hp <cheolyeing@gmail.com>
2020-05-04 21:08:54 +0900

    PropTypse install

--- src/App.js
+++ src/App.js
@@ -1,24 +1,53 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import PropType from "prop-types";
+
+const list = [
+  {
+    id: 1,
+    place: "바다",
+    img:
+      "https://cdn.crowdpic.net/detail-thumb/thumb_d_C6386337D543E5BD60DB8168D08F5CFA.jpg",
+  },
+  {
+    id: 2,
+    place: "산",
+    img:
+      "https://lh3.googleusercontent.com/proxy/3FwPnhdCI9tf0pA4MpME-JMLXNrBNulJgqg9d3Esn_5RGaVLzT93NEpVQciFt-s3rArDXkzQSDeUWO-tjI-zpEFfswbAPzo3aejC0sc78MOZX-tQaagQDjwJWJRNmuAa0F2KtuICL9VLn6SEOgeBXQ",
+  },
+  {
+    id: 3,
+    place: "도시",
+    img:
+      "https://img.hani.co.kr/imgdb/resize/2018/0518/152653444301_20180518.JPG",
+  },
+];
+
+function Place({ id, place, img }) {
+  return (
+    <div>
+      <h2>
+        {id} . {place}
+      </h2>
+      <img src={img} alt={place} />
+      <br />
+      <br />
+    </div>
+  );
+}
+
+Place.PropType = {
+  place: PropType.string.isRequired,
+  img: PropType.string.isRequired,
+};
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>장소와 사진</h1>
+      {list.map((item) => (
+        <Place key={item.id} id={item.id} place={item.place} img={item.img} />
+      ))}
+      ;
     </div>
   );
 }
